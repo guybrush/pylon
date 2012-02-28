aa2332a9d6751cce2041d3f6eb9dfa282a466425
Patrick Pfeiffer <patrick@buzzle.at>
2012-02-28 08:19:54 +0100

    cli

--- bin/cli.js
+++ bin/cli.js
@@ -3,78 +3,63 @@
 var pylon = require('../')
   , opti = require('optimist')
   , argv = opti.argv
-  , _pkg = require('../package.json')
-  , _help = {}
-  , _config = {}
-  , _conn
-  , _server
+  , pkg = require('../package.json')
+  , help = {}
+  , cfg = {}
+  , server
   
-_config.port = Math.floor(Math.random() * 40000 + 10000)
-_config.host = '0.0.0.0'
-_config.remotes = {local:{port:_config.port,host:_config.host}}
+cfg.port = Math.floor(Math.random() * 40000 + 10000)
+cfg.host = '0.0.0.0'
+cfg.remotes = {local:{port:cfg.port,host:cfg.host}}
   
-_help.logo = 
+help.logo = 
 [ '           _'
 , ' ___  _ _ | | ___  ___'
 , '| . || | || || . ||   |'
 , '|  _||_  ||_||___||_|_|'
-, '|_|  |___|v'+_pkg.version
+, '|_|  |___|v'+pkg.version
 ].join('\n')
   
-_help.intro =
-[ _help.logo
+help.intro =
+[ help.logo
 , ''
-, 'pylon [-r <remote>] [-c <path to configFile>] [<command> [<options>]]'
+, 'pylon [-r <remote> [-c <cfgFile>]] [-p <port> [-h <host>]] <cmd> [<opts>]'
 , ''
 , 'commands:'
 , ''
-, '   version   .. print version-number'
-, '   config    .. get/set config'
-, '   start     .. start the pylon-server (this only works without -r)'
-, '   set       .. set entries'
-, '   get       .. get entries'
-, '   del       .. delete entries'
-, '   subscribe .. subscribe to events'
-, '   help      .. try `pylon help <command>` for more info'
-, ''
+, '   version .. print version-number'
+, '   config  .. print config'
+, '   start   .. start the pylon-server (this only works without -r)'
+, '   keys    .. query for keys with regexp'
+, '   on      .. subscribe to events'
+, '   help    .. try `pylon help <command>` for more info'
 ].join('\n')
 
-_help.version =
-[ ''
+help.version =
+[ 'pylon [-r <remote> [-c <cfgFile>]] [-p <port> [-h <host>]] version'
 , ''
+, 'examples:'
+, ''
+, '   pylon version .. print version of local installed pylon'
+, '   pylon -r a1f  .. print version of remote running pylon'
+, '   pylon -p 3000 -h 234.345.456.567 version .. print version of remote pylon..'
 ].join('\n')
 
-_help.config =
-[ ''
-, ''
+help.start =
+[ 'pylon start -p <port> [-h <host>]'
 ].join('\n')
 
-_help.start =
-[ ''
+
+help.on =
+[ 'pylon [-r <remote> [-c <cfgFile>]] [-p <port> [-h <host>]] on <event>'
 , ''
+, 'examples:'
+, ''
+, '   pylon -r a1f on "set * balancer"'
+, '   pylon -r a1b on "**"'
 ].join('\n')
 
-_help.set =
-[ ''
-, ''
-].join('\n')
-
-_help.get =
-[ ''
-, ''
-].join('\n')
-
-_help.del =
-[ ''
-, ''
-].join('\n')
-
-_help.subscribe =
-[ ''
-, ''
-].join('\n')
-
-if (!argv._[0]) return exit(null, _help.intro)
+if (!argv._[0]) return exit(null, help.intro)
   
 parseArgs()
 
@@ -83,16 +68,16 @@ function parseArgs() {
   var cmd = argv._.shift()
   switch (cmd) {
     case 'version':
-      exit(null,_pkg.version)
+      exit(null,pkg.version)
       break
     case 'config':
-      exit(null,_config)
+      exit(null,cfg)
     case 'start':
       var opts = {}
-      opts.port = _config.port
-      opts.host = _config.host
-      _server = pylon().listen(opts,function(){})
-      _server.on('ready',function(){
+      opts.port = cfg.port
+      opts.host = cfg.host
+      server = pylon().listen(opts,function(){})
+      server.on('ready',function(){
         console.log('pylon is listening on '+opts.host+':'+opts.port)
       })
       break
@@ -109,9 +94,9 @@ function parseArgs() {
       pylon.connect()
       break
     case 'help':
-      if (!argv._[0] || !_help[argv._[0]])
-        return exit(null, _help.intro)
-      exit(null, _help[argv._[0]])
+      if (!argv._[0] || !help[argv._[0]])
+        return exit(null, help.intro)
+      exit(null, help[argv._[0]])
       break
     default:
       exit('unknown command: '+cmd)
@@ -119,8 +104,7 @@ function parseArgs() {
 }
 
 function exit(err,msg) {
-  _conn && _conn.end()
-  _server && _server.close()
+  server && server.close()
   if (err) console.error(err)
   else console.log(msg)
   process.exit(0)
